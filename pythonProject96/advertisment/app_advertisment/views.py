c5ceaa6ddf8800946c1780b00d49afd1421a53ba
Nik <ktoto4022@gamil.com>
2023-07-18 20:16:26 +1000

    initial commit

--- pythonProject96/advertisment/app_advertisment/views.py
+++ pythonProject96/advertisment/app_advertisment/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("Communication attempt 2")
 def index(request):
-    return HttpResponse("Communication attempt 2")
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
